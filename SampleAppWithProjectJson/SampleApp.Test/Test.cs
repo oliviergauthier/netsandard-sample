3ec2387db453b34c6ea401100fe53256b90a834c
Olivier Gauthier <ogauthier@betomorrow.com>
2017-06-07 12:31:05 +0200

    Ajout du projet de test

---
+++ SampleAppWithProjectJson/SampleApp.Test/Test.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+using System;
+namespace SampleApp.Test {
+    [TestFixture()]
+    public class Test {
+        [Test()]
+        public void TestCase() {
+        }
+    }
+}
